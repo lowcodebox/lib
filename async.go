6da61c85bf8ceeee4dbf88b998591928d6ceedab
loveckiy.ivan <loveckiy.ivan@wildberries.ru>
2023-05-15 11:38:09 +0300

    upd signature Recover

--- async.go
+++ async.go
@@ -14,18 +14,15 @@ func RunAsync(ctx context.Context, fn func()) {
 	}()
 }
 
-func Recover(ctx context.Context) bool {
+func Recover(ctx context.Context) (flag bool, msg string) {
 	recoverErr := recover()
 	if recoverErr == nil {
-		return false
+		return false, ""
 	}
 
 	stack := debug.Stack()
 	pc, file, line, _ := runtime.Caller(2)
-	msg := fmt.Sprintf("Recovered panic. file: %s, line: %d, function: %s, error: %s, stack: %s", file, line, runtime.FuncForPC(pc).Name(), recoverErr, stack)
+	msg = fmt.Sprintf("Recovered panic. file: %s, line: %d, function: %s, error: %s, stack: %s", file, line, runtime.FuncForPC(pc).Name(), recoverErr, stack)
 
-	// TODO заменить на логгер
-	fmt.Println(msg)
-
-	return true
+	return true, msg
 }
