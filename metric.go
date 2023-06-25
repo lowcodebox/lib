09ba9f20f1fb13e67770b56924a46f6659b8197b
loveckiy.ivan <loveckiy.ivan@wildberries.ru>
2023-06-25 21:31:51 +0300

    clear logger from metric (deplicated)

--- metric.go
+++ metric.go
@@ -2,7 +2,6 @@ package lib
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 	"sort"
 	"sync"
@@ -292,7 +291,7 @@ func (s *serviceMetric) Middleware(next http.Handler) http.Handler {
 }
 
 // interval - интервалы времени, через которые статистика будет сбрасыватсья в лог
-func NewMetric(ctx context.Context, logger Log, interval time.Duration) (metrics ServiceMetric) {
+func NewMetric(ctx context.Context, logger any, interval time.Duration) (metrics ServiceMetric) {
 	m := sync.Mutex{}
 	t := StateHost{}
 	s := Metrics{
@@ -321,7 +320,7 @@ func NewMetric(ctx context.Context, logger Log, interval time.Duration) (metrics
 	return metrics
 }
 
-func RunMetricLogger(ctx context.Context, m ServiceMetric, logger Log, interval time.Duration) {
+func RunMetricLogger(ctx context.Context, m ServiceMetric, logger any, interval time.Duration) {
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 
@@ -334,8 +333,8 @@ func RunMetricLogger(ctx context.Context, m ServiceMetric, logger Log, interval 
 			m.Generate()    // сгенерировали метрики
 			m.SaveToStash() // сохранили в карман
 			m.Clear()       // очистили объект метрик для приема новых данных
-			mes, _ := json.Marshal(m.Get())
-			logger.Trace(string(mes)) // записали в лог из кармана
+			//mes, _ := json.Marshal(m.Get())
+			//logger.Trace(string(mes)) // записали в лог из кармана
 
 			ticker = time.NewTicker(interval)
 		}
