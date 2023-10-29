52fb451c2f27a829ca1c7fb4f48707781540155a
loveckiy.ivan <loveckiy.ivan@wildberries.ru>
2023-10-29 21:27:42 +0300

    [upd] add templates path

--- pkg/service/storage.go
+++ pkg/service/storage.go
@@ -8,7 +8,7 @@ import (
 
 // Alive ...
 func (s *service) Storage(ctx context.Context, in model.StorageIn) (out model.StorageOut, err error) {
-	if in.Bucket != "upload" && in.Bucket != "assets" {
+	if in.Bucket != "upload" && in.Bucket != "templates" && in.Bucket != "assets" {
 		out.Body, out.MimeType, err = s.vfs.ReadFromBucket(in.File, in.Bucket) // читаем из заданного бакета (в данном случае только для templates)
 	} else {
 		out.Body, out.MimeType, err = s.vfs.Read(in.File)
