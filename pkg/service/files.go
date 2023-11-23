e4e23617b9e0bca9b2ad346cf8fd1d724987e6d8
loveckiy.ivan <loveckiy.ivan@wildberries.ru>
2023-11-23 15:47:59 +0300

    [upd] rem files

--- pkg/service/files.go
+++ pkg/service/files.go
@@ -2,14 +2,8 @@ package service
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"path/filepath"
-	"strings"
 
 	"git.lowcodeplatform.net/fabric/app/pkg/model"
-	"git.lowcodeplatform.net/packages/logger"
-	"go.uber.org/zap"
 )
 
 // Files ...
@@ -22,35 +16,35 @@ func (s *service) Files(ctx context.Context, in model.ServiceFilesIn) (out model
 }
 
 func loadFileOnly(ctx context.Context, in model.ServiceFilesIn) (out model.ServiceFilesOut, err error) {
-	var path = ""
+	//var path = ""
 
-	// добавляем переданный путь
-	getPath := r.FormValue("path")
-	getName := r.FormValue("name")
-
-	urlPath := strings.Split(getPath, sep)
-	for _, v := range urlPath {
-		path = filepath.Join(path, v)
-	}
-
-	// полный путь к файлу
-	thisFilePath := path + sep + getName
-	objResp.Status.Description = thisFilePath
-
-	err = h.vfs.Write(thisFilePath, []byte{})
-	if err != nil {
-		logger.Error(h.ctx, "error vfs Write", zap.Error(err))
-		objResp.Status.Error = fmt.Sprintf("error vfs Write, err: %s", err)
-	}
-
-	// формируем ответ
-	outByte, err := json.Marshal(objResp)
-	if err != nil {
-		out = fmt.Sprint(err)
-		return
-	}
-
-	out = string(outByte)
+	//// добавляем переданный путь
+	//getPath := r.FormValue("path")
+	//getName := r.FormValue("name")
+	//
+	//urlPath := strings.Split(getPath, sep)
+	//for _, v := range urlPath {
+	//	path = filepath.Join(path, v)
+	//}
+	//
+	//// полный путь к файлу
+	//thisFilePath := path + sep + getName
+	//objResp.Status.Description = thisFilePath
+	//
+	//err = h.vfs.Write(thisFilePath, []byte{})
+	//if err != nil {
+	//	logger.Error(h.ctx, "error vfs Write", zap.Error(err))
+	//	objResp.Status.Error = fmt.Sprintf("error vfs Write, err: %s", err)
+	//}
+	//
+	//// формируем ответ
+	//outByte, err := json.Marshal(objResp)
+	//if err != nil {
+	//	out = fmt.Sprint(err)
+	//	return
+	//}
+	//
+	//out = string(outByte)
 
 	return
 }
