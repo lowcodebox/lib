e832a5dd81201ad4fcc7d84dd3ee6a96e8d7325c
cherkasov <dsasadf13@gmail.com>
2024-05-27 13:43:52 +0300

    [upd] change fields to consts

--- pkg/servers/httpserver/monitoring.go
+++ pkg/servers/httpserver/monitoring.go
@@ -10,23 +10,29 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+const (
+	methodField     = "method"
+	httpMethodField = "http_method"
+	statusCodeField = "status_code"
+)
+
 var (
 	timingMetrics metrics.Histogram = kitprometheus.NewHistogramFrom(prometheus.HistogramOpts{
 		Name:    "app_request_http_timing",
 		Help:    "timing a request in http server",
 		Buckets: []float64{0.05, 0.1, 0.2, 0.5, 1, 10},
-	}, []string{"method", "http_method"})
+	}, []string{methodField, httpMethodField})
 
 	statusCodeMetrics = kitprometheus.NewCounterFrom(prometheus.CounterOpts{
 		Name: "app_request_http_status",
 		Help: "return status code from http server",
-	}, []string{"method", "http_method", "status_code"})
+	}, []string{methodField, httpMethodField, statusCodeField})
 )
 
 // monitoringTiming собирает информацию о времени выполнения запроса
 func (*httpserver) monitoringTiming(start time.Time, method, httpMethod string) {
 	timingMetrics.
-		With("method", method, "http_method", httpMethod).
+		With(methodField, method, httpMethodField, httpMethod).
 		Observe(time.Since(start).Seconds())
 }
 
@@ -38,6 +44,6 @@ func (*httpserver) monitoringStatusCode(method, httpMethod string, statusCode in
 
 	codeStr := strconv.Itoa(statusCode)
 	statusCodeMetrics.
-		With("method", method, "http_method", httpMethod, "status_code", codeStr).
+		With(methodField, method, httpMethodField, httpMethod, statusCodeField, codeStr).
 		Add(1)
 }
