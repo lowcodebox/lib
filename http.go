c765cac17d463d150f6f3e42458f560894cdaa04
Wmuga <dsasadf13@gmail.com>
2024-10-08 11:18:37 +0300

    add response wrapper

--- http.go
+++ http.go
@@ -17,6 +17,18 @@ import (
 	"github.com/labstack/gommon/color"
 )
 
+// ResponseWrapper Обертка над ResponseWriter для сбора статус кодов
+type ResponseWrapper struct {
+	http.ResponseWriter
+
+	code int
+}
+
+func (r *ResponseWrapper) WriteHeader(statusCode int) {
+	r.code = statusCode
+	r.ResponseWriter.WriteHeader(statusCode)
+}
+
 const clientHttpTimeout = 60 * time.Second
 
 var (
